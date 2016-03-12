03bc85991f6b70015284f72fbb9ac5a191678cba
song <wjsdlrtm@gmail.com>
2016-03-13 01:23:03 +0900

    working

--- client/module/landing/config/landing.routes.js
+++ client/module/landing/config/landing.routes.js
@@ -1,51 +1,60 @@
-(function() {
+(function () {
 
-  angular
-    .module('landing')
-    .config(config);
+    angular
+        .module('landing')
+        .config(config);
 
-  /* @ngInject */
-  function config($stateProvider) {
+    /* @ngInject */
+    function config($stateProvider) {
 
-    // Home state routing
-    $stateProvider
-        .state('home', {
-          url: '/',
-        abstract: true,
-          views: {
-            contentView: {
-              controller: 'landingController',
-              templateUrl: 'module/landing/views/landing.html',
-              
-            }
-          }
-        })
-        .state('home.main', {
-          url: 'main',
-          views: {
-            mainView: {
-              controller: 'landingController',
-              templateUrl: 'module/landing/views/landing.main.html'
-            }
-          }
-        })
-    .state('home.upload', {
-          url: 'upload',
-          views: {
-            mainView: {
-              controller: 'landingUploadController',
-              templateUrl: 'module/landing/views/upload.html'
-            }
-          }
-        })
-        .state('home.signup', {
-          url: 'signup',
-          views: {
-            mainView: {
-              controller: 'landingController',
-              templateUrl: 'module/landing/views/landing.signup.html'
-            }
-          }
-        });;
-  }
+        // Home state routing
+        $stateProvider
+            .state('home', {
+                url: '/',
+                abstract: true,
+                views: {
+                    contentView: {
+                        controller: 'landingController',
+                        templateUrl: 'module/landing/views/landing.html',
+
+                    }
+                }
+            })
+            .state('home.main', {
+                url: 'main',
+                views: {
+                    mainView: {
+                        controller: 'landingController',
+                        templateUrl: 'module/landing/views/landing.main.html'
+                    }
+                }
+            })
+            .state('home.myclass', {
+                url: 'myclass',
+                views: {
+                    mainView: {
+                        controller: 'landingController',
+                        templateUrl: 'module/landing/views/myclass.html'
+                    }
+                }
+            })
+            .state('home.upload', {
+                url: 'upload',
+                views: {
+                    mainView: {
+                        controller: 'landingUploadController',
+                        templateUrl: 'module/landing/views/upload.html'
+                    }
+                }
+            })
+            .state('home.signup', {
+                url: 'signup',
+                views: {
+                    mainView: {
+                        controller: 'landingController',
+                        templateUrl: 'module/landing/views/landing.signup.html'
+                    }
+                }
+            });;
+    }
 })();
